4c7d79655e41f9206bb4260b8021702326e8c4b2
Admin <Admin@MSI>
2024-04-19 11:25:07 +0700

    Add project files.

---
+++ NhaThuocLongChauFPT/Data/ProductSupplier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NhaThuocLongChauFPT.Data;
+
+public partial class ProductSupplier
+{
+    public int ProductId { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
---
+++ NhaThuocLongChauFPT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhaThuocLongChauFPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
